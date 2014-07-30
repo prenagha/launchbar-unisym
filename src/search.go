ca9a02227bfdb3e690fa832c167108559da23923
Nima Jahanshahi <nbjahan@gmail.com>
2014-07-30 17:01:00 +0430

    Better search results

--- src/search.go
+++ src/search.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sort"
+	"strings"
 
 	. "github.com/nbjahan/go-launchbar"
 	"github.com/nbjahan/gofuzz"
@@ -11,20 +12,33 @@ var scores []float64
 
 type byScore Items
 
-func (items byScore) Len() int           { return len(items) }
-func (items byScore) Swap(i, j int)      { items[i], items[j] = items[j], items[i] }
-func (items byScore) Less(i, j int) bool { return scores[i] < scores[j] }
+func (items byScore) Len() int      { return len(items) }
+func (items byScore) Swap(i, j int) { items[i], items[j] = items[j], items[i] }
+func (items byScore) Less(i, j int) bool {
+	if scores[i] == scores[j] {
+		return items[i].Item().Title < items[j].Item().Title
+	}
+	return scores[i] < scores[j]
+}
 
 func search(q string) *Items {
-
+	items := NewItems()
+	q = strings.TrimSpace(q)
+	if len(q) < 3 {
+		return items
+	}
+	names := make(map[string]struct{})
 	fuzz := gofuzz.New()
 	fuzz.Threshold = 0.2
-	items := NewItems()
 	scores = make([]float64, 0)
 	for cat, symbols := range chars {
 		for _, row := range symbols {
 			pos, score := fuzz.Search(row[0], q, 0)
 			if pos != -1 {
+				if _, found := names[row[0]]; found {
+					continue
+				}
+				names[row[0]] = struct{}{}
 				scores = append(scores, score)
 				i := NewItem(row[0])
 				i.SetIcon(iconPath(cat, row[0]))
