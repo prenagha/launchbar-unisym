4d6f670d7614bd0882a17ecc64edc0d7c167a4f2
Nima Jahanshahi <nbjahan@gmail.com>
2014-07-31 13:05:11 +0430

    Add the ability to paste the chars in frontmost apps

--- src/unisym.go
+++ src/unisym.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"path"
 	"strings"
 
@@ -25,15 +26,29 @@ func init() {
 	})
 }
 
+func pasteChar(s string) {
+	exec.Command("osascript", "-e", fmt.Sprintf(`tell application "LaunchBar"
+       perform action "Paste in Frontmost Application" with string "%s"
+       end tell`, s)).Start()
+}
+
 var funcs = FuncMap{
+	"pasteChar": func(c *Context) {
+		args := c.Input.FuncArgsMapString()
+		pasteChar(args[0])
+	},
 	"showChar": func(c *Context) Items {
 		args := c.Input.FuncArgsMapString()
+		if c.Action.IsShiftKey() || c.Action.IsControlKey() || c.Action.IsOptionKey() || c.Action.IsCommandKey() {
+			pasteChar(args[1])
+			return Items(nil)
+		}
 		items := NewItems()
 		utf8 := fmt.Sprintf("%X", args[1])
 		unicode := fmt.Sprintf("%X", []rune(args[1])[0])
-		items.Add(NewItem(args[1]).SetSubtitle(args[0]).SetIcon("at.obdev.launchbar:Words"))
-		items.Add(NewItem(unicode).SetSubtitle("unicode").SetIcon("at.obdev.launchbar:InfoTemplate"))
-		items.Add(NewItem(utf8).SetSubtitle("utf8").SetIcon("at.obdev.launchbar:InfoTemplate"))
+		items.Add(NewItem(args[1]).SetSubtitle(args[0]).SetIcon("at.obdev.launchbar:Words").Run("pasteChar", args[1]).SetAction("unisym"))
+		items.Add(NewItem(unicode).SetSubtitle("unicode").SetIcon("at.obdev.launchbar:InfoTemplate").Run("pasteChar", unicode).SetAction("unisym"))
+		items.Add(NewItem(utf8).SetSubtitle("utf8").SetIcon("at.obdev.launchbar:InfoTemplate").Run("pasteChar", utf8).SetAction("unisym"))
 		return *items
 	},
 }
